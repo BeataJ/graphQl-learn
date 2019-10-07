721db658d55f3b0407aca7cf519586727e635d1b
beata <beata@emc2wave.com>
2019-10-07 15:24:52 -0700

    add express-graphql

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}))
 app.listen(5000, () => console.log('server is running port 5000'))
