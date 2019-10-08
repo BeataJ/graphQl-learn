480bbdee72711e636735584775273d7932f5c29b
beata <beata@emc2wave.com>
2019-10-07 22:04:55 -0700

    add RootQueryType , BookType, shema

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const expressGraphQL = require('express-graphql');
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull
 } = require('graphql')
 app = express();
 
@@ -24,7 +27,31 @@ const books = [
   { id: 8, name: 'Beyond the Shadows', authorId: 3 }
 ] 
 
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  description: 'This represents a book written by an author',
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt)},
+    name: {type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt)}
+  })
+})
 
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: 'List of All Books',
+      resolve: () => books
+    }
+  })
+})
+
+const schema = new GraphQLSchema({
+  query: RootQueryType
+})
 
 app.use('/graphql', expressGraphQL({
   schema: schema,
